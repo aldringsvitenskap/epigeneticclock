ab976d44b773f894d45c04200931689cad3a8c5b
Espen Riskedal <espen@cutehacks.com>
2017-08-25 10:42:18 +0200

    refactor: use observeEvent(..) in a more granular way
    - added the methylation reactive value
    - observe file and text input separately
    - observe methylation$Data and re-calculate
      methylation age when it changes
    - read .csv in a tryCatch(..) for robustness

--- app.R
+++ app.R
@@ -32,63 +32,72 @@ server <- function(input, output) {
   options(shiny.maxRequestSize = 200 * 1024 ^ 2)
   source("horvath2013.R")
   
-  observe({
-    if ((!is.null(input$methylationFile)) || (!is.null(input$gsmID) && nchar(input$gsmID) > 3)) {
-      methylationTable = NULL;
-      methylationMessage = NULL;
-      methylationDetail = NULL;
-      methylationDescription = NULL;
-      
-      if (!is.null(input$methylationFile)) {
-        inputFileName <- input$methylationFile[[1, 'name']]
-        inputFileSize <- input$methylationFile[[1, 'size']]
-        inputFilePath <- input$methylationFile[[1, 'datapath']]
-        
-        methylationMessage <- sprintf("Processing %s", inputFileName)
-        methylationDetail <- sprintf("%d bytes", inputFileSize)
-        methylationDescription <- "Empty..."
-        methylationTable <- read.csv.sql(inputFilePath)
+  methylation <- reactiveValues(Table  = NULL, Message = NULL, Detail = NULL, Description = NULL)
+  
+  observeEvent(input$gsmID, {
+    if (!is.null(input$gsmID) && nchar(input$gsmID) > 3) {
+      gsmTable <-  tryCatch({
+        getGEO(input$gsmID)
+      }, error = function(e) {
+        NULL
+      })
+      methylation$Message <- sprintf("Processing %s", input$gsmID)
+      methylation$Detail <- "Unknown size"
+      if (!is.null(gsmTable)) {
+        methylation$Description <- head(Meta(gsmTable))
+        methylation$Table <- Table(gsmTable)[,1:2]
       } else {
-        gsmTable <-  tryCatch({
-          getGEO(input$gsmID)
-        }, error = function(e) {
-          NULL
-        })
-        methylationMessage <- sprintf("Processing %s", input$gsmID)
-        methylationDetail <- "Unknown size"
-        if (!is.null(gsmTable)) {
-          methylationTable <- Table(gsmTable)[,1:2]
-          methylationDescription <- head(Meta(gsmTable))
-        }
+        methylation$Description <- NULL
+        methylation$Table <- NULL
       }
-      
-      withProgress(
-        message = methylationMessage,
-        detail = methylationDetail,
-        value = 0,
-        {
-          setProgress(1/20)
-          ageResults = tryCatch({
-            calculateAge(methylationTable)
-          }, error = function(e) {
-            paste("Error: ", e$message)
-          })
-          setProgress(1)
-        }
-      )
-      
-      output$table <- renderTable({ageResults},  
-                                  striped = TRUE,  
-                                  rownames = TRUE,
-                                  caption = "Estimaged DNAm age",
-                                  caption.placement = getOption("xtable.caption.placement", "top"))
-      output$logfile <- renderText(read_file("LogFile.txt"))
-      output$description <- renderTable({methylationDescription},
-                                        striped = TRUE,  
-                                        rownames = TRUE,
-                                        caption = "GSM metadata",
-                                        caption.placement = getOption("xtable.caption.placement", "top"))
     }
+  })
+
+  observeEvent(input$methylationFile, {
+    inputFileName <- input$methylationFile[[1, 'name']]
+    inputFileSize <- input$methylationFile[[1, 'size']]
+    inputFilePath <- input$methylationFile[[1, 'datapath']]
+    
+    methylation$Message <- sprintf("Processing %s", inputFileName)
+    methylation$Detail <- sprintf("%d bytes", inputFileSize)
+    methylation$Description <- "Empty..."
+    methylation$Table <- tryCatch({
+      read.csv.sql(inputFilePath)
+    }, error = function(e) {
+      NULL
+    })
+  })
+
+  observeEvent(methylation$Table, {
+    if (file.exists("LogFile.txt")) {
+      file.remove("LogFile.txt")
+    }
+    withProgress(
+      message = methylation$Message,
+      detail = methylation$Detail,
+      value = 0,
+      {
+        setProgress(1/20)
+        ageResults = tryCatch({
+          calculateAge(methylation$Table)
+        }, error = function(e) {
+          paste("Error: ", e$message)
+        })
+        setProgress(1)
+      }
+    )
+
+    output$table <- renderTable({ageResults},  
+                                striped = TRUE,  
+                                rownames = TRUE,
+                                caption = "Estimaged DNAm age",
+                                caption.placement = getOption("xtable.caption.placement", "top"))
+    output$logfile <- renderText(read_file("LogFile.txt"))
+    output$description <- renderTable({methylation$Description},
+                                      striped = TRUE,  
+                                      rownames = TRUE,
+                                      caption = "GSM metadata",
+                                      caption.placement = getOption("xtable.caption.placement", "top"))
   })
 }
 
