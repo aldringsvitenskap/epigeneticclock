0ee70bbeb2974dfea045b4bb8e6e49a25700927b
Espen Riskedal <espen@cutehacks.com>
2017-08-25 13:19:55 +0200

    feat: added button to calculate
    - rearranged help and input text
    - added sample name in result output

--- app.R
+++ app.R
@@ -9,47 +9,49 @@ library(GEOquery)
 
 # Define UI
 ui <- fluidPage(titlePanel("Epigenetic clock"),
-                
+
                 sidebarLayout(sidebarPanel(
+                  helpText("Upload a .csv file. The first column has the CpG cites, the following column(s) has methylation values"),
                   fileInput(
                     inputId = "methylationFile",
-                    label = "Choose methylation csv file",
+                    label = "Upload methylation .csv file (max 200MB)",
                     accept = c('text/csv',
                                'text/comma-separated-values,text/plain',
                                '.csv'),
                     multiple = FALSE
                   ),
-                  helpText("Supports .csv files where the first column lists the cpg cites, and the following columns list their methylation values"),
-                  textInput(inputId = "gsmID", label = "GSM sample ID", placeholder = "GSMxxx"),
-                  helpText("Enter a GSM id from the Geo Expression Omnibus and wait for processing to start"),
+                  tags$hr(),
+                  helpText("Enter a GEO sample id (for example GSM401945) and press the button"),
+                  textInput(inputId = "gsmID", label = "Enter GSM sample ID", placeholder = "GSMXXXX"),
+                  actionButton(inputId = "calculate", label = "Predict age"),
+                  tags$hr(),
                   tags$a(href="https://doi.org/10.1186/gb-2013-14-10-r115", "Based on \"DNA methylation age of human tissues and cell types\" by Steve Horvath")
                 ),
-                
+
                 mainPanel(tableOutput("table"), tableOutput("description"), textOutput("logfile"))))
 
 # Define server
 server <- function(input, output) {
   options(shiny.maxRequestSize = 200 * 1024 ^ 2)
   source("horvath2013.R")
-  
-  methylation <- reactiveValues(Table  = NULL, Message = NULL, Detail = NULL, Description = NULL)
-  
-  observeEvent(input$gsmID, {
-    if (!is.null(input$gsmID) && nchar(input$gsmID) > 3) {
-      gsmTable <-  tryCatch({
-        getGEO(input$gsmID)
-      }, error = function(e) {
-        NULL
-      })
-      methylation$Message <- sprintf("Processing %s", input$gsmID)
-      methylation$Detail <- "Unknown size"
-      if (!is.null(gsmTable)) {
-        methylation$Description <- head(Meta(gsmTable))
-        methylation$Table <- Table(gsmTable)[,1:2]
-      } else {
-        methylation$Description <- NULL
-        methylation$Table <- NULL
-      }
+
+  methylation <- reactiveValues(Table  = NULL, Message = NULL, Detail = NULL, Description = NULL, Name = NULL)
+
+  observeEvent(input$calculate, {
+    gsmTable <-  tryCatch({
+      getGEO(input$gsmID)
+    }, error = function(e) {
+      NULL
+    })
+    methylation$Name <- input$gsmID
+    methylation$Message <- sprintf("Processing %s", input$gsmID)
+    methylation$Detail <- "Unknown size"
+    if (!is.null(gsmTable)) {
+      methylation$Description <- head(Meta(gsmTable))
+      methylation$Table <- Table(gsmTable)[,1:2]
+    } else {
+      methylation$Description <- NULL
+      methylation$Table <- NULL
     }
   })
 
@@ -57,7 +59,8 @@ server <- function(input, output) {
     inputFileName <- input$methylationFile[[1, 'name']]
     inputFileSize <- input$methylationFile[[1, 'size']]
     inputFilePath <- input$methylationFile[[1, 'datapath']]
-    
+
+    methylation$Name <- inputFileName
     methylation$Message <- sprintf("Processing %s", inputFileName)
     methylation$Detail <- sprintf("%d bytes", inputFileSize)
     methylation$Description <- "Empty..."
@@ -84,14 +87,14 @@ server <- function(input, output) {
       }
     )
 
-    output$table <- renderTable({ageResults},  
-                                striped = TRUE,  
+    output$table <- renderTable({ageResults},
+                                striped = TRUE,
                                 rownames = TRUE,
-                                caption = "Estimaged DNAm age",
+                                caption = paste("Estimaged DNAm age of ", methylation$Name),
                                 caption.placement = getOption("xtable.caption.placement", "top"))
     output$logfile <- renderText(read_file("LogFile.txt"))
     output$description <- renderTable({methylation$Description},
-                                      striped = TRUE,  
+                                      striped = TRUE,
                                       rownames = TRUE,
                                       caption = "GSM metadata",
                                       caption.placement = getOption("xtable.caption.placement", "top"))
